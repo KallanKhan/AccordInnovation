d367f65b8910969472bfa58f2c74de2d1530425e
KallanKhan <kallan.khan@gmail.com>
2025-10-13 20:37:24 +0400

    Add cbsc100

--- AccordInnovations/src/main/java/com/accordInnovations/controller/DepartmentController.java
+++ AccordInnovations/src/main/java/com/accordInnovations/controller/DepartmentController.java
@@ -46,7 +46,7 @@ public class DepartmentController {
 	public ResponseEntity<Page<Department>> getAllDepartments(Pageable pageable) {
         System.out.println("pageable");
 		System.out.println("pageable");
-
+        System.out.println("getAllDepartments");
 		Page<Department> departmentPage = departmentService.findAll(pageable);
         return ResponseEntity.ok(departmentPage);
     }
